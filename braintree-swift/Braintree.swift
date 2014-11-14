162aeb28116fec7a9c630ddfc9949ce973508144
Mickey Reiss <mickey@venmo.com>
2014-11-14 10:31:08 -0800

    Initial commit
    * Xcode template project

---
+++ braintree-swift/Braintree.swift
@@ -0,0 +1,9 @@
+//
+//  Braintree.swift
+//  braintree-swift
+//
+//  Created by Mickey Reiss on 11/14/14.
+//  Copyright (c) 2014 Braintree. All rights reserved.
+//
+
+import Foundation
---
+++ braintree-swift/braintree-swift.h
@@ -0,0 +1,19 @@
+//
+//  braintree-swift.h
+//  braintree-swift
+//
+//  Created by Mickey Reiss on 11/14/14.
+//  Copyright (c) 2014 Braintree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for braintree-swift.
+FOUNDATION_EXPORT double braintree_swiftVersionNumber;
+
+//! Project version string for braintree-swift.
+FOUNDATION_EXPORT const unsigned char braintree_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <braintree_swift/PublicHeader.h>
+
+
